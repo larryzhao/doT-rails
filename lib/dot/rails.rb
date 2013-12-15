a7e1122f2530ea2b19719bc0999e0af516e5c0ee
Larry Zhao <thehiddendepth@gmail.com>
2013-12-15 22:09:49 +0800

    register dot

--- lib/dot/rails.rb
+++ lib/dot/rails.rb
@@ -3,6 +3,7 @@ require "dot/rails/version"
 module DoT
   module Rails
     class Engine < ::Rails::Engine
+      Application.assets.register_engine '.dot', Sprockets::DotTemplate
     end
   end
 end
