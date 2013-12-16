4596d19c44eb611ef0826e45ba7a429e42295497
Larry Zhao <thehiddendepth@gmail.com>
2013-12-16 12:23:43 +0800

    removed

--- vendor/assets/javascripts/doT.runtime.js
+++
@@ -1,7 +0,0 @@
-String.prototype.encodeHTML = function() {
-  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': '&#34;', "'": '&#39;', "/": '&#47;' },
-      matchHTML = /&(?!#?\w+;)|<|>|"|'|\//g;
-  return function() {
-    return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;
-  };
-}();
